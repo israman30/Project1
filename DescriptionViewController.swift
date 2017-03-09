31f947bd56b83b206986f1f54d1ddabbccfaf231
Israel Manzo <israman814@gmail.com>
2017-03-09 14:12:36 -0500

    Updating alert and saving on description controller

--- DescriptionViewController.swift
+++ DescriptionViewController.swift
@@ -48,7 +48,6 @@ class DescriptionViewController: UIViewController {
     // MARK: Alert function when message is saved
     func alert(){
         let alert = UIAlertController(title: "Yay!", message: "It's saved", preferredStyle: .alert)
-        alert.addAction(UIAlertAction(title: "Cancel", style: .cancel, handler: nil))
         alert.addAction(UIAlertAction(title: "Ok", style: .default, handler: nil))
         present(alert, animated: true, completion: nil)
     }
